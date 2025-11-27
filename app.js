0cadea2d63eebf12317a502c2581f0ae8eb4a476
Tien-MECI <tech@meci.vn>
2025-11-27 14:12:34 +0700

    sửa nội dung baocaolotrinh

--- app.js
+++ app.js
@@ -3617,7 +3617,7 @@ async function calculateAverageFuelPrice(month, year) {
 
         if (rowMonth === month && rowYear === year) {
           const donGia = parseFloat(row[11]); // Cột 11 (index 10) là đơn giá
-          const loaiNhienLieu = row[8]; // Cột 10 (index 9) là loại nhiên liệu
+          const loaiNhienLieu = row[9]; // Cột 10 (index 9) là loại nhiên liệu
 
           if (!isNaN(donGia) && donGia > 0) {
             totalPrice += donGia;
