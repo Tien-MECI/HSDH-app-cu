2ad737f66454320ba03a6a995aa824ac7523d0cd
Tien-MECI <tech@meci.vn>
2026-01-06 09:41:26 +0700

    hoadonnhap.ejs

--- app.js
+++ app.js
@@ -2070,7 +2070,7 @@ app.get("/taohoadon-:madh", async (req, res) => {
       const totalAfterTax = parseFloat(row[totalAfterTaxIndex]) || 0;// Thành tiền sau thuế
 
       // 👉 Tính toán lại theo chuẩn kế toán
-      const amount = amountchuathue / (1 + taxRate / 100);            // đơn giá chưa thuế
+      const amount = amountchuathue;            // đơn giá chưa thuế
       const unitPrice = amount * quantity;        // thành tiền chưa thuế
       const taxAmount = unitPrice * (taxRate / 100);                    // Tiền thuế GTGT
 
