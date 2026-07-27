53b7bfb3f69340082eb128c67b81adbc5bb85fff
Tien-MECI <tech@meci.vn>
2026-07-27 16:30:33 +0700

    app.js Lenhpvc, BBNT, bbgn

--- app.js
+++ app.js
@@ -3699,7 +3699,7 @@ app.get("/baogiapvc/:maDonHang-:soLan", async (req, res) => {
         // --- Lấy chi tiết sản phẩm PVC ---
         const ctRes = await sheets.spreadsheets.values.get({
             spreadsheetId: SPREADSHEET_ID,
-            range: "Don_hang_PVC_ct!A10000:AC",
+            range: "Don_hang_PVC_ct!A8000:AC",
         });
         const ctRows = (ctRes.data.values || []).slice(1);
 
@@ -4371,7 +4371,7 @@ app.get("/bbgn/:maDonHang-:soLan", async (req, res) => {
         // --- Chi tiết sản phẩm ---
         const ctRes = await sheets.spreadsheets.values.get({
             spreadsheetId: SPREADSHEET_ID,
-            range: "Don_hang_PVC_ct!A10000:AC",
+            range: "Don_hang_PVC_ct!A8000:AC",
         });
         const ctRows = (ctRes.data.values || []).slice(1);
 
@@ -4678,7 +4678,7 @@ app.get("/bbnt/:maDonHang-:soLan", async (req, res) => {
         // --- Chi tiết sản phẩm ---
         const ctRes = await sheets.spreadsheets.values.get({
             spreadsheetId: SPREADSHEET_ID,
-            range: "Don_hang_PVC_ct!A10000:AC",
+            range: "Don_hang_PVC_ct!A8000:AC",
         });
         const ctRows = (ctRes.data.values || []).slice(1);
 
